04a7d939354f0aeeb70e1e2ce0b0dc0d790e5a15
Brock Cooper <brockcooper@gmail.com>
2015-10-12 14:24:00 +0000

    Add ability to create new Fishing Report

--- app/controllers/fishing_reports_controller.rb
+++ app/controllers/fishing_reports_controller.rb
@@ -5,4 +5,22 @@ class FishingReportsController < ApplicationController
     @flies = @fishing_report.flies.all
     @hatches = @fishing_report.hatches.all
   end
+
+  def new
+    @fishing_report = current_user.fishing_reports.new
+  end
+
+  def create
+    @fishing_report = current_user.fishing_reports.build(fishing_report_params)
+    if @fishing_report.save
+      flash[:success] = "Fishing Report created!"
+    end
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def fishing_report_params
+    params.require(:fishing_report).permit(:title, :date, :location, :body_of_water, :story, :weather, :air_temp, :water_flow, :visibility, :water_temp, :rod_used, :line_used, :leader_used, :tippet_used, :best_time_of_day, :best_water_type, :advice, :other_notes )
+  end
 end
