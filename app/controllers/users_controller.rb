7117f553225ea427b9f29bc058456747aad81289
Brock Cooper <brockcooper@gmail.com>
2015-10-21 04:13:04 +0000

    Add About Me and Stats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,19 @@ class UsersController < ApplicationController
     @fishing_reports = @user.fishing_reports.all
   end
 
+  def update
+    if current_user.update_attributes(user_params)
+     flash[:notice] = "User information updated"
+   else
+     flash[:error] = "Invalid user information"
+   end
+    redirect_to user_path
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :email)
+    params.require(:user).permit(:username, :email, :about_me)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006131309) do
+ActiveRecord::Schema.define(version: 20151021032423) do
 
   create_table "fish", force: :cascade do |t|
     t.integer  "user_id"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20151006131309) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.text     "about_me"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
