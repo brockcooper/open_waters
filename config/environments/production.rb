4d09bc955ed7e30c6400b27817e80bfd84b45f0e
Brock Cooper <brockcooper@gmail.com>
2015-11-01 23:54:26 +0000

    Fix devise emails on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'openwaters.heroku.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 end
