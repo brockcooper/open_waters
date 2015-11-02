292a34092720cc043788f68f5aaa47552f9a8d56
Brock Cooper <brockcooper@gmail.com>
2015-11-02 00:24:15 +0000

    Add styles to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  
+  config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true 
 end
